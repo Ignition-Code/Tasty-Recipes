9354075451428e019bc3601ccc235423b0dcd17c
Ignition-Code <not-available>
2022-12-13 19:40:57 -0300

    Register
    - Add register user activity and functions

--- app/src/main/java/com/ms/tastyrecipes/RegisterUserActivity.java
+++ app/src/main/java/com/ms/tastyrecipes/RegisterUserActivity.java
@@ -1,14 +1,35 @@
 package com.ms.tastyrecipes;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ms.tastyrecipes.database.Controller;
+import com.ms.tastyrecipes.entities.User;
 
 public class RegisterUserActivity extends AppCompatActivity {
+
+    Controller controller;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_user);
+        EditText etMail = findViewById(R.id.etMail);
+        EditText etNaturalName = findViewById(R.id.etNaturalName);
+        EditText etUsernameRegister = findViewById(R.id.etUsernameRegister);
+        EditText etPasswordRegister = findViewById(R.id.etPasswordRegister);
+        Button btSaveRegister = findViewById(R.id.btSaveRegister);
+        btSaveRegister.setOnClickListener(v -> save(etUsernameRegister.getText().toString(), etPasswordRegister.getText().toString(), etNaturalName.getText().toString(), etMail.getText().toString()));
+        controller = Room.databaseBuilder(getApplicationContext(), Controller.class, "recipes").build();
+    }
+
+    void save(String username, String password, String naturalName, String mail) {
+        Controller.UserDao userDao = controller.userDao();
+        userDao.insertUser(new User(null, username, password, naturalName, mail));
+        finish();
     }
 }
